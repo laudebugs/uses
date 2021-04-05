8e402958be2fb3692a2c231b5fe039a700b973af
lbugasu <lbugasu@gmail.com>
2021-04-04 20:09:51 -0400

    updating setup file

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # run as root
 # sudo -i
-cd ..
+cd 
 # Work from a setup folder
 mkdir setup && cd setup
-sudo apt-get update
+sudo apt-get update -y
 sudo apt upgrade -y
 sudo apt install wget -y
 
 # install Snapd (https://snapcraft.io/docs/installing-snap-on-ubuntu)
-sudo apt update
+sudo apt update -y
 sudo apt install snapd
+
+# install curl
+sudo apt install curl
 
 # Git
 echo Installing git...
@@ -26,11 +29,42 @@ read github_email
 git config user.email = $github_email
 
 ## Node
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+sudo apt-get install nodejs
+# Yarn
+sudo apt remove cmdtest
+sudo apt autoremove
+sudo npm install -g yarn
+# To solve the error of: there are no scenarios (https://stackoverflow.com/questions/53471063/yarn-error-there-are-no-scenarios-must-have-at-least-one)
+sudo apt remove yarn -y
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
+# g++ 
+sudo apt-get install g++
 # gcc
 sudo apt install build-essential
+
 # Python3 
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update -y
+sudo apt-get install python3
+
 # Install Node
 sudo snap install node --classic
+
+# Kite
+bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)"
+
+# Java jre
+sudo apt-get install default-jre
+
+# Nautilus
+sudo apt-get update -y
+sudo apt-get install -y nautilus
 
 # Browsers - Google Chrome, Chromium, Brave
 # install google chrome
@@ -41,6 +75,21 @@ sudo snap install chromium
 # install Brave (https://brave.com/linux/)
 sudo snap install brave
 
+## COMMAND LINE INTERFACES
+# Heroku
+sudo snap install --classic heroku
+# React
+sudo npm i -g react
+# Angular
+sudo npm install -g @angular/cli
+# NX
+sudo npm i -g nx
+# AWS
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+# Emma (https://github.com/maticzav/emma-cli)
+sudo npm i -g emma-cli
 
 ## Code Editors
 # VS Code (https://code.visualstudio.com/docs/setup/linux)
@@ -48,7 +97,12 @@ sudo snap install --classic code
 # IDEA
 sudo snap install intellij-idea-community --classic
 
+# Android Studio
+sudo snap install android-studio --classic
 
+# Android Devide Bridge
+sudo apt-get install android-tools-adb android-tools-fastboot
+sudo adb start-server
 ## TOOLS
 # CMake
 # Slack 
@@ -69,9 +123,21 @@ sudo snap install postman
 # Insomnia
 sudo snap install insomnia
 
+# MongoDB
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update -y
+sudo apt-get install -y mongodb-org
 # MongoDB Compass
 wget https://downloads.mongodb.com/compass/mongodb-compass_1.26.0_amd64.deb
 sudo dpkg -i mongodb-compass_1.26.0_amd64.deb
+
+# ddrescue (https://www.tecmint.com/create-bootable-ubuntu-usb-disk/)
+sudo apt install gddrescue
+
+# Redis
+sudo snap install redis
 
 # Signal
 sudo snap install signal-desktop
@@ -92,6 +158,8 @@ sudo apt install vim
 # VLC
 sudo snap install vlc
 
+# Unzip
+sudo apt install unzip
 
 ## Fonts
 
@@ -102,11 +170,61 @@ fc-cache -f -v
 ## CUSTOMIZATIONS
 # Terminal
 # Gogh (https://github.com/Mayccoll/Gogh)
-sudo apt-get install dconf-cli uuid-runtime
-bash -c "$(wget -qO- https://git.io/vQgMr)" 
+# Uncomment for interractive mode
+# sudo apt-get install dconf-cli uuid-runtime
+# bash -c "$(wget -qO- https://git.io/vQgMr)" 
+## Non-interactive mode
+# clone the repo into "$HOME/src/gogh"
+mkdir -p "$HOME/src"
+cd "$HOME/src"
+git clone https://github.com/Mayccoll/Gogh.git gogh
+cd gogh/themes
+
+# necessary on ubuntu
+export TERMINAL=gnome-terminal
+
+# install themes
+./zenburn.sh
+./dracula.sh
+./sweet.sh
+./homebrew.sh
+./lavandula.sh
+
+cd ~/setup/
 # SURU++ (https://github.com/gusbemacbe/suru-plus)
 wget -qO- https://raw.githubusercontent.com/gusbemacbe/suru-plus/master/install.sh | sh
 # SURU + Folders (https://gusbemacbe.github.io/suru-plus-folders/en/)
 wget -qO- https://git.io/fhQdI | sh
 # Set folders to 90ssummer
 suru-plus-folders -C 90ssummer --theme Suru++
+# Gnome Tweak Tool
+sudo apt install gnome-tweak-tool -y
+# Grub Customizer
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt update -y
+sudo apt install grub-customizer -y
+# Dracula (https://draculatheme.com/gtk)
+wget -O dracula-theme.zip https://github.com/dracula/gtk/archive/master.zip
+unzip dracula-theme.zip -d dracula-theme
+mkdir Dracula 
+sudo mv dracula-theme/gtk-master/* Dracula/
+# Ant Standard Buttons
+wget -O Ant-Nebula-Standard-Buttons.tar https://github.com/EliverLara/Ant-Nebula/releases/download/v1.3.0/Ant-Nebula-standard-buttons.tar
+mkdir Ant-standard-buttons
+mkdir Ant_standard_buttons
+tar -xvf Ant-standard-buttons.tar -C Ant_standard_buttons/
+mv Ant_standard_buttons/Ant/* Ant-standard-buttons/
+sudo mv Ant-standard-buttons /usr/share/themes/
+# Ant Nebula
+wget -O Ant-Nebula.tar https://github.com/EliverLara/Ant-Nebula/releases/download/v1.3.0/Ant-Nebula.tar
+tar -xvf Ant-Nebula.tar
+sudo mv Ant-Nebula /usr/share/themes/
+
+# Shell Extendions
+sudo apt install gnome-shell-extensions
+
+## DRIVERS 
+# Nvidia 
+sudo apt install nvidia-driver-450
+# Synaptic
+sudo apt-get install xserver-xorg-input-synaptics
