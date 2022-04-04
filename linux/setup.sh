a5c88dbd015a5c664b11b5e63847af973af9034b
lbugasu <lbugasu@gmail.com>
2022-04-04 12:25:49 -0500

    fix: edit linux setup script

--- linux/setup.sh
+++ linux/setup.sh
@@ -11,6 +11,6 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 
-git clone https://github.com/lbugasu/uses.git
+git clone https://github.com/lbugasu/uses.git && cd uses
 
 ansible-playbook linux/setup.yml
